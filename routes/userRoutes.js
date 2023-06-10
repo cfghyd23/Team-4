e689fa18f21b5f9fb3e714c7311d825965e8842f
Jahnavi5989 <bjahnavi5989@gmail.com>
2023-06-11 04:20:07 +0530

    default password implementation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,7 +16,7 @@ router.delete('/delete/user', authenticateToken, (req, res) => {
           return res.status(404).json({ error: 'User not found' });
         }
   
-        // Optionally, you can also invalidate the token here if desired
+       
   
         res.status(200).json({ message: 'User deleted successfully' });
       })
@@ -24,6 +24,39 @@ router.delete('/delete/user', authenticateToken, (req, res) => {
         res.status(500).json({ error: 'Failed to delete user' });
       });
   });
+
+  router.delete('/delete/user', authenticateToken, (req, res) => {
+    const userId = req.user.id;
+  
+    User.findById(userId)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+  
+        // Check if the user is an admin
+        if (!user.isAdmin) {
+          return res.status(401).json({ error: 'Only admin users can delete users' });
+        }
+  
+        // Proceed with user deletion
+        User.findByIdAndDelete(userId)
+          .then((deletedUser) => {
+            if (!deletedUser) {
+              return res.status(404).json({ error: 'User not found' });
+            }
+  
+            res.status(200).json({ message: 'User deleted successfully' });
+          })
+          .catch((error) => {
+            res.status(500).json({ error: 'Failed to delete user' });
+          });
+      })
+      .catch((error) => {
+        res.status(500).json({ error: 'Failed to retrieve user' });
+      });
+  });
+  
 
 router.get('/user', authenticateToken, (req, res) => {
     const userId = req.user.id;
