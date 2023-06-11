342433b452e70e4aea9cd4e5e864f5cc7ab5e244
Yashvardhan Arora <yash22arora@gmail.com>
2023-06-11 07:30:16 +0530

    Setup axios
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/components/Dashboard/Overview.js
+++ client/src/components/Dashboard/Overview.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PieChart from "../piechart1";
 import PieChart2 from "../piechart2";
 import Container from "react-bootstrap/Container";
@@ -6,6 +6,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export default function Overview() {
+  useEffect(() => {}, []);
   const data = {
     name: "Sanju",
     campaigns: [
@@ -20,7 +21,7 @@ export default function Overview() {
 
   const internDetails = ["Username, password"];
   return (
-    <div>
+    <div className="w-full flex flex-col items-center p-12">
       <Container>
         <Row>
           <Col sm={7}>
@@ -35,11 +36,17 @@ export default function Overview() {
           </Col>
         </Row>
       </Container>
-      <div className="flex flex-row items-center justify-between my-0 mx-8">
-        <h4 className="">Current Fundraiser Cause: {data.campaigns[0].name}</h4>
-        <span>Funds Collected: ₹{data.campaigns[0].fundscollected}</span>
-        <span>Next Milestone: ₹{data.campaigns[0].fundstonextmilestone}</span>
+      <div className="flex flex-row items-center justify-between my-8 mx-8 w-full">
+        <h5 className="">Current Fundraiser Cause: {data.campaigns[0].name}</h5>
+        <h5>Funds Collected: ₹{data.campaigns[0].fundscollected}</h5>
+        <h5>Next Milestone: ₹{data.campaigns[0].fundstonextmilestone}</h5>
       </div>
+      <span className="p-6 rounded-lg backdrop-blur-md bg-gray-500 bg-opacity-10 mt-4 mx-auto">
+        <span className="font-semibold text-orange-600">
+          🔗 Fundraiser URL:{" "}
+        </span>
+        https://payments-test.cashfree.com/forms/intern1-campaign1
+      </span>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./app");
 require("dotenv").config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
